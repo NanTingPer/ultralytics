27f98573ba2143525797581487ac2f5b074c596f
Nan_Ting <a3380991070@163.com>
2025-09-02 08:23:13 +0800

    upd

--- _ultralytics/CBAM.py
+++ _ultralytics/CBAM.py
@@ -10,29 +10,40 @@ class CBAM(nn.Module):
     在 ultralytics 的 __init__ 中  __all__ => add "CBAM"
     在 修改 ultralytics/cfg/models/11/yolo11.yaml
     """
-    def __init__(self, c1, ksize=7):
+
+    def __init__(self, c1, kernel_size=7):
+        """
+        Initialize CBAM with given parameters.
+        Args:
+            c1 (int): Number of input channels.
+            kernel_size (int): Size of the convolutional kernel for spatial attention.
+        """
         super().__init__()
         self.channel_attention = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(c1 , c1 // 16, kernel_size=1, bias=False),
+            nn.Conv2d(c1, c1 // 16, kernel_size=1, bias=False),
             nn.ReLU(inplace=True),
             nn.Conv2d(c1 // 16, c1, kernel_size=1, bias=False),
             nn.Sigmoid()
         )
-
         self.spatial_attention = nn.Sequential(
-            nn.Conv2d(2, 1, kernel_size=ksize, padding=ksize // 2, bias=False),
+            nn.Conv2d(2, 1, kernel_size=kernel_size, padding=kernel_size // 2, bias=False),
             nn.Sigmoid()
         )
+        # self.channel_attention = ChannelAttention(c1)
+        # self.spatial_attention = SpatialAttention(kernel_size)
 
     def forward(self, x):
-        ch_att = self.channel_attention(x)
-        x = x * ch_att
-        avg_pool = torch.mean(x, dim=1, keepdim=True)
-        max_pool = torch.max(x, dim=1, keepdim=True)[0]
-        t_cat = torch.cat([avg_pool, max_pool], dim=1)
-        sp_input = self.spatial_attention(t_cat)
-        sp_att = x * sp_input
-        x = x * sp_att
-        return x
-
+        """
+        Apply channel and spatial attention sequentially to input tensor.
+        Args:
+            x (torch.Tensor): Input tensor.
+        Returns:
+            (torch.Tensor): Attended output tensor.
+        """
+        # return self.spatial_attention(self.channel_attention(x))
+        x = x * self.channel_attention(x)
+        pool_avg = torch.mean(x, dim=1, keepdim=True)
+        pool_max, _ = torch.max(x, dim=1, keepdim=True)
+        pool_cat = torch.cat([pool_avg, pool_max], dim=1)
+        return x * self.spatial_attention(pool_cat)
